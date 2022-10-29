779768faa809b121959ed6a597338c54420a7aba
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-10-29 13:55:35 +0600

    home_detail_kyrgyz_language added

---
+++ lib/src/modules/home/presentation/views/home_detail/home_detail_kyrgyz_language/logic/home_detail_kyrgyz_language_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'home_detail_kyrgyz_language_state.dart';
+
+class HomeDetailKyrgyzLanguageCubit extends Cubit<HomeDetailKyrgyzLanguageState> {
+  HomeDetailKyrgyzLanguageCubit() : super(HomeDetailKyrgyzLanguageInitial());
+}
---
+++ lib/src/modules/home/presentation/views/home_detail/home_detail_kyrgyz_language/logic/home_detail_kyrgyz_language_state.dart
@@ -0,0 +1,10 @@
+part of 'home_detail_kyrgyz_language_cubit.dart';
+
+abstract class HomeDetailKyrgyzLanguageState extends Equatable {
+  const HomeDetailKyrgyzLanguageState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class HomeDetailKyrgyzLanguageInitial extends HomeDetailKyrgyzLanguageState {}
