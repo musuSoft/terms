2ac7f05f61d89583ae3ec2f1dbda485e36d0178e
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-10-31 11:37:01 +0600

    home_detail_biology

---
+++ lib/src/modules/home/presentation/views/home_detail/home_detail_biology/logic/home_detail_biology_state.dart
@@ -0,0 +1,10 @@
+part of 'home_detail_biology_cubit.dart';
+
+abstract class HomeDetailBiologyState extends Equatable {
+  const HomeDetailBiologyState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class HomeDetailBiologyInitial extends HomeDetailBiologyState {}
