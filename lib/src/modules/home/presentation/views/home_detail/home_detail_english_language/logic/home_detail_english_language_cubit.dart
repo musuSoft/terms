13c6d52068cb7eb5f9b9fcf2fcb3a25de2f84b8a
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-11-05 00:10:10 +0600

    term updated

--- lib/src/modules/home/presentation/views/home_detail/home_detail_english_language/logic/home_detail_english_language_cubit.dart
+++ lib/src/modules/home/presentation/views/home_detail/home_detail_english_language/logic/home_detail_english_language_cubit.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 
 part 'home_detail_english_language_state.dart';
 
-class HomeDetailEnglishLanguageCubit extends Cubit<HomeDetailEnglishLanguageState> {
+class HomeDetailEnglishLanguageCubit
+    extends Cubit<HomeDetailEnglishLanguageState> {
   HomeDetailEnglishLanguageCubit() : super(HomeDetailEnglishLanguageInitial());
 }
